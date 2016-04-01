6b59bb98201c633ff41c882e7c4815201b389fa2
Mark Staebler <markstaebler@gmail.com>
2016-03-31 18:05:59 -0700

    add recipe feature

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var tempRecipe = {};
 var recipes = [
     {
         name: "Martini",
@@ -9,7 +10,20 @@ var recipes = [
 ];
 
 class RecipeBox extends React.Component{
-  render() {
+
+    handleChange(name, e){
+        name === "ingredients" ? tempRecipe[name] = e.target.value.split(',') : tempRecipe[name] = e.target.value;
+    }
+    submit(e) {
+        recipes.push(tempRecipe);
+        console.log(recipes);
+        e.preventDefault();
+        ReactDOM.render(
+          <RecipeBox recipes={recipes} name="recipe name" ingredients="ingredients" />,
+          document.getElementById('content')
+        );
+    }
+    render() {
       var recipeList = this.props.recipes.map((recipe)=>{
           return(
           <div className="recipe">
@@ -27,28 +41,19 @@ class RecipeBox extends React.Component{
       <div className="recipeBox">
         Recipe Box
         {recipeList}
-        <AddRecipe />
-      </div>
-    );
-  }
-}
-
-class AddRecipe extends React.Component{
-    handleClick : () =>{
-        
-    },
-  render() {
-    return (
-      <div className="addRecipe">
-      <button onClick={this.handleClick}>Add Recipe</button>
-      <input defaultValue="recipe name here"></input>
-      <input defaultValue="ingredient1, ingredient2, .."></input>
+        <div className="addRecipe">
+        <form onSubmit={this.submit.bind(this)}>
+          <button> Add Recipe</button>
+        </form>
+        <input defaultValue={this.props.name} onChange={this.handleChange.bind(this, "name")} ></input>
+        <input defaultValue={this.props.ingredients} onChange={this.handleChange.bind(this, "ingredients")} ></input>
+        </div>
       </div>
     );
   }
 }
 
 ReactDOM.render(
-  <RecipeBox recipes={recipes} />,
+  <RecipeBox recipes={recipes} name="recipe name" ingredients="ingredients" />,
   document.getElementById('content')
 );
